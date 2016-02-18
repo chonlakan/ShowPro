82a99ffe9947b4e038ac4704e0f26d9f8c43044c
Max <chonrakan56@gmail.com>
2016-02-18 15:30:31 +0700

    start18/02/2559

--- app/src/main/java/pbru/yaemsak/chonlakan/showpro/ServiceActivity.java
+++ app/src/main/java/pbru/yaemsak/chonlakan/showpro/ServiceActivity.java
@@ -8,6 +8,7 @@ public class ServiceActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_service);
     }
 }
