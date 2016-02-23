6ec90f6b8ad46be5b056ac016d9d942966d6a932
Max <chonrakan56@gmail.com>
2016-02-23 13:49:45 +0700

    Create promotion listview finish

--- app/src/main/java/pbru/yaemsak/chonlakan/showpro/PromotionActivity.java
+++ app/src/main/java/pbru/yaemsak/chonlakan/showpro/PromotionActivity.java
@@ -1,5 +1,7 @@
 package pbru.yaemsak.chonlakan.showpro;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,7 +20,39 @@ public class PromotionActivity extends AppCompatActivity {
         //Bind Widget
         promotionListView = (ListView)findViewById(R.id.listView2);
 
+        //CreateListView
+        CreateListView();
+
     }//Main method
+
+    private void CreateListView() {
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.DATABASE_NAME,
+                MODE_PRIVATE, null);
+
+        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM " + ManagTABLE.TABLE_promotion, null);
+        cursor.moveToFirst();
+
+        String[] namePromotion = new String[cursor.getCount()];
+        String[] picPromotion = new String[cursor.getCount()];
+        String[] startPromotion = new String[cursor.getCount()];
+        String[] endPromotion = new String[cursor.getCount()];
+
+        for (int i = 0; i < cursor.getCount(); i++) {
+
+            namePromotion[i] = cursor.getString(cursor.getColumnIndex(ManagTABLE.COLUMN_NamePromotion));
+            picPromotion[i] = cursor.getString(cursor.getColumnIndex(ManagTABLE.COLUMN_PictPromotion));
+            startPromotion[i] = cursor.getString(cursor.getColumnIndex(ManagTABLE.COLUMN_TimeStart));
+            endPromotion[i] = cursor.getString(cursor.getColumnIndex(ManagTABLE.COLUMN_TimeEnd));
+
+        }//for
+        cursor.close();
+
+        //create listview
+        PromoteAdapter promoteAdapter = new PromoteAdapter(PromotionActivity.this,
+                picPromotion, namePromotion, startPromotion, endPromotion);
+        promotionListView.setAdapter(promoteAdapter);
+
+    }//create list view
 
     public void clickBackPromotion(View view) {
         finish();
