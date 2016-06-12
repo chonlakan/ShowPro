9e61e79f2643335c57cfb529a2c40cb9608bf765
Max <chonrakan56@gmail.com>
2016-06-12 14:06:31 +0700

    Create Marker finish

--- app/src/main/java/pbru/yaemsak/chonlakan/showpro/LocationMapsActivity.java
+++ app/src/main/java/pbru/yaemsak/chonlakan/showpro/LocationMapsActivity.java
@@ -5,10 +5,13 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.speech.tts.Voice;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.drive.internal.StringListResponse;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -16,6 +19,12 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class LocationMapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -24,12 +33,16 @@ public class LocationMapsActivity extends FragmentActivity implements OnMapReady
     private Criteria criteria;//แกนโลก
     private boolean GPSABoolean, networkABoolean;
     private double myLatADouble, myLngADouble;
+    private static double centerLatADouble = 14.089958;
+    private static double centerLngADouble = 100.067562;
+    private static final String urlPlace = "http://chonlakan.com/max/get_place.php";
+    private SynLatLng synLatLng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -147,43 +160,89 @@ public class LocationMapsActivity extends FragmentActivity implements OnMapReady
 
     }//openServiceLocation
 
+    //
+    public class SynLatLng extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(urlPlace).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                 Log.d("8June", "error DoIn ==> " + e.toString());
+                return null;
+            }
+        }//DoInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            try {
+
+                Log.d("8June", "JSON ==> " + s);
+
+                JSONArray jsonArray = new JSONArray(s);
+
+                String[] PlaceStrings = new String[jsonArray.length()];
+                String[] LatStrings = new String[jsonArray.length()];
+                String[] LngStrings = new String[jsonArray.length()];
+                String[] PhoneString = new String[jsonArray.length()];
+
+
+                for (int i=0;i<jsonArray.length();i++) {
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                    PlaceStrings[i] = jsonObject.getString("Place");
+                    LatStrings[i] = jsonObject.getString("Lat");
+                    LngStrings[i] = jsonObject.getString("Lng");
+                    PhoneString[i] = jsonObject.getString("Phone");
+
+                    //Create All Marker
+                    double lat = Double.parseDouble(LatStrings[i]);
+                    double lng = Double.parseDouble(LngStrings[i]);
+
+                    LatLng latLng = new LatLng(lat, lng);
+                    mMap.addMarker(new MarkerOptions()
+                            .position(latLng)
+                            .title("Place")
+                            .snippet("Phone")
+                            .icon(BitmapDescriptorFactory.fromResource(R.drawable.mak1)));
+
+                }   //for
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }//onPreE
+    }//SynLatLng
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        mMap = googleMap;
 
-        mMap.setMyLocationEnabled(true);
         mMap.setMapType(googleMap.MAP_TYPE_HYBRID);
 
-        LatLng yonghouse = new LatLng(13.975795, 99.659115);
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().isCompassEnabled();
+        mMap.getUiSettings().setScrollGesturesEnabled(true);
+        mMap.getUiSettings().setMapToolbarEnabled(true);
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(yonghouse, 10));
+        //Setup Center
+        LatLng latLng = new LatLng(centerLatADouble, centerLngADouble);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 7));
+
+        SynLatLng synLatLng = new SynLatLng();
+        synLatLng.execute();
 
 
-        mMap.addMarker(new MarkerOptions()
-                .title("บริษัท ยงเฮ้าส์ จำกัด")
-                .snippet("สาขา ท่าม่วง  โทร.034-626603-12")
-                .position(yonghouse));
-
-       /* // Add a marker Current Location and move the camera
-        LatLng CurrentLocation = new LatLng(myLatADouble, myLngADouble);
-        mMap.addMarker(new MarkerOptions().position(CurrentLocation)
-        //.title("Your Current Location")
-        .icon(BitmapDescriptorFactory.fromResource(R.drawable.mar2)));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(CurrentLocation));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(CurrentLocation, 13)); // ระยะ zoom
-        mMap.setMapType(googleMap.MAP_TYPE_HYBRID);*/
-
-        // Add a marker in Sydney and move the camera
-       /* LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions()
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.mar2))
-                .position(sydney)
-                .title("Marker in Sydney"));
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15));
-        mMap.setMapType(googleMap.MAP_TYPE_HYBRID);*/
-
-    }//on already
-
+    }//onMap
 }//Main Class
